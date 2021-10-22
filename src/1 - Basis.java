9388e90a94844bea1b036730cd414e479f05ea96
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-10-23 00:33:40 +0100

    Upload last changes

---
+++ src/1 - Basis.java
@@ -0,0 +1,6 @@
+class Basis {
+
+    public static void HelloWorld () {
+        System.out.println("Hello World in another file");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main (String[] args) {
-        System.out.print("Hello World");
+        System.out.println("Hello World");
+        Basis.HelloWorld();
     }
 }
