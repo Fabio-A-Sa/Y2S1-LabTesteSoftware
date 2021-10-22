ffd55a5347712b5f0cfb6a63fee6d39a60571d33
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-10-23 00:48:41 +0100

    Inputs

--- src/1 - Basis.java
+++ src/1 - Basis.java
@@ -1,11 +1,25 @@
+import java.util.Scanner; // To scan user inputs
+
 class Basis {
 
     public static void run () {
         HelloWorld();
+        Inputs();
     }
 
     public static void HelloWorld () {
         System.out.println("Hello World!");
+    }
+
+    public static void Inputs () {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Your name: ");
+        String name = input.nextLine();
+        System.out.print("Your age: ");
+        int age = input.nextInt();
+        System.out.println("Your name is " + name + " and you have " + age + " years!");
+
     }
 
 
