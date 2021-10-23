343c10ef37ca8440c45f5026b9b9ea741e58dbad
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2021-10-23 12:50:12 +0100

    Inputs

--- src/1 - Basis.java
+++ src/1 - Basis.java
@@ -5,6 +5,7 @@ class Basis {
     public static void run () {
         HelloWorld();
         Inputs();
+        Calculator();
     }
 
     public static void HelloWorld () {
@@ -17,10 +18,26 @@ class Basis {
         System.out.print("Your name: ");
         String name = input.nextLine();
         System.out.print("Your age: ");
-        int age = input.nextInt();
-        System.out.println("Your name is " + name + " and you have " + age + " years!");
+        int age = 0;
+
+        while (age <= 0 || age > 120) {
+            try {
+                age = input.nextInt();
+                if (age <= 0 || age > 120) {
+                    System.out.print("Error. Please try again:\nYour age: ");
+                }
+            } catch (Exception exception) {
+                input.nextLine();
+                System.out.print("Error. Please try again:\nYour age: ");
+            }
+        }
+
+        input.nextLine();
+        System.out.println("Your name is " + name + " and you have " + age + " years old!");
 
     }
 
-
+    public static void Calculator() {
+        System.out.println("something");
+    }
 }
