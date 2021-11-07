1ff670ad54fd7368d071cee0ad52da354442bed5
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-07 14:20:55 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -1,3 +1,11 @@
 // Created on October, 2021
 // @author: Fábio Araújo de Sá
 
+public class Main {
+
+    public static void main (String[] args) {
+        //InputOutput.run();
+        //DataStructures.run();
+        OOP.run();
+    }
+}
