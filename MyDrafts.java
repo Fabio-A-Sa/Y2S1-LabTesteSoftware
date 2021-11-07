a412444362da74d87dd0de33fc08c04a42aa0fb0
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-07 14:22:07 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -286,3 +286,43 @@ class Bike extends Vehicle {
 
     public boolean haveWindows() { return windows > 0 ; }
 }
+import java.io.File;
+        import java.io.FileNotFoundException;
+        import java.io.FileReader;
+        import java.io.IOException;
+
+class Files {
+
+    public static void run() {
+        UsingFiles();
+    }
+
+    public static void UsingFiles () {
+
+        String name = "Lusiadas.txt";
+        File file = new File(name);
+        if (file.exists()) {
+            System.out.println("\n\nFile " + name + " exists");
+
+            // Extracts data and show content
+            try {
+                FileReader reader = new FileReader(name);
+                int data = reader.read();
+
+                System.out.println("\n" + name + ": \n");
+                while (data != -1) {
+                    System.out.print((char)data);
+                    data = reader.read();
+                }
+                reader.close();
+            } catch (FileNotFoundException exception) {
+                exception.printStackTrace();
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+
+        } else {
+            System.out.println("File not found");
+        }
+    }
+}
