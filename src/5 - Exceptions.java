e10d4c5f9d37d6c2f3811e78e959ba94bd16ef28
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-14 17:02:28 +0000

    Exceptions run

--- src/5 - Exceptions.java
+++ src/5 - Exceptions.java
@@ -6,5 +6,7 @@ class Exceptions {
     public static void run() {
         System.out.println("Run");
     }
+
+    
 }
 
--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main (String[] args) {
         // InputOutput.run();
         // DataStructures.run();
-        OOP.run();
+        // OOP.run();
+        Exceptions.run();
     }
 }
