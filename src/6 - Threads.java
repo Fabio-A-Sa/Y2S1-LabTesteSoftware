40a5c5de2b1ca645e07c81478b887cb166b4b5f9
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-14 21:06:15 +0000

    Threads

--- src/6 - Threads.java
+++ src/6 - Threads.java
@@ -1,6 +1,41 @@
+// Created on November, 2021
+// @author: Fábio Araújo de Sá
+
 class usingThreads {
 
     public static void run() {
-        System.out.println("Threads run!");
+
+        Model m = new Model();
+        View v = new View();
+
+        new Thread() {
+            public void run () {
+                while (m.a < 1000) {
+                    m.increment();
+                }
+            }
+        }.start();
+
+        new Thread() {
+            public void run() {
+                while (m.a < 1000) {
+                    v.draw(m);
+                }
+            }
+        }.start();
+    }
+}
+
+class Model {
+    int a = 0, b = 0;
+    public void increment() {
+        a++;
+        b++;
+    }
+}
+
+class View {
+    public void draw (Model m) {
+        System.out.println(m.a + " " + m.b);
     }
 }
