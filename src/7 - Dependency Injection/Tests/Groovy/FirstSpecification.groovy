53e8baaa216c13c161c10db6e422904c7442bfd3
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-24 23:18:58 +0000

    DRfats

---
+++ src/7 - Dependency Injection/Tests/Groovy/FirstSpecification.groovy
@@ -0,0 +1,26 @@
+class FirstSpecification extends Specification {
+
+    def "one plus one should equal to" () {
+        expect:
+        1 + 1 == 2
+    }
+
+    def "two plus two should equal four" () {
+        given:
+            int left = 2
+            int right = 2
+        when:
+            int result = left + right
+        then:
+            result == 4
+    }
+
+    def "Should be able to remove from list" () {
+        given:
+            def list = [1, 2, 3, 4]
+        when:
+            list.remove(0)
+        then:
+            list == [2, 3, 4]
+    }
+}
