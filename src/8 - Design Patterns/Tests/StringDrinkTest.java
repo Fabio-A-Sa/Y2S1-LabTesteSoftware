5b2f41db5c787be6f45778bf50c71a4146be0972
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 21:18:22 +0000

    More tests

--- src/8 - Design Patterns/Tests/StringDrinkTest.java
+++ src/8 - Design Patterns/Tests/StringDrinkTest.java
@@ -206,4 +206,22 @@ public class StringDrinkTest {
         client.wants(drink, recipe, stringBar);
         assertEquals("dCbX-DcBa", drink.getText());
     }
+
+    @Test
+    public void smartStrategyStartOpened() {
+
+        StringBar stringBar = new StringBar();
+        StringDrink drink = new StringDrink("AbCd-aBcD");
+        StringRecipe recipe = stringBar.getRecipe();
+
+        SmartStrategy strategy = new SmartStrategy();
+        HumanClient client = new HumanClient(strategy);
+
+        // Recipe is ordered immediately as happy hour was already under way
+        stringBar.startHappyHour();
+        client.wants(drink, recipe, stringBar);
+        assertEquals("dCbX-DcBa", drink.getText());
+    }
+
+    
 }
