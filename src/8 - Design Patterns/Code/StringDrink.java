6d3d6be6c1df23a2159f010f8c2786547c721eb0
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 19:32:06 +0000

    Design Pattern file

---
+++ src/8 - Design Patterns/Code/StringDrink.java
@@ -0,0 +1,17 @@
+public class StringDrink {
+
+    private String text;
+
+    public StringDrink(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
