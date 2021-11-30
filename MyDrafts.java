e1d1df1f747073882ef51ca1e01bf42b88bf09c4
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 22:27:12 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -550,3 +550,28 @@ public class ListFilter {
         return result;
     }
 }
+
+/**
+ * An utility class to sort list of numbers.
+ */
+public class ListSorter implements GenericListSorter {
+
+    /**
+     * Really stupid way to sort a list.
+     * @return A sorted version of the list.
+     */
+    @Override
+    public List<Integer> sort(List<Integer> list) {
+        List<Integer> sorted = new ArrayList<>();
+        for (Integer number : list)
+            sorted.add(number);
+
+        for (int i = 0; i < sorted.size() - 1; i++)
+            for (int j = i + 1 ; j < sorted.size() ; j++)
+                if (sorted.get(i) > sorted.get(j))
+                    Collections.swap(sorted, i, j);
+
+        return sorted;
+    }
+}
+
