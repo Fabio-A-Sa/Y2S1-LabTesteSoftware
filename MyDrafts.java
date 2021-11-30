6735f23b1c2df45ffe8f9397b528bf62f47d19f0
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 22:23:34 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -333,3 +333,45 @@ class Exceptions {
         System.out.println("Run");
     }
 }
+
+class playWithExceptions {
+
+    public static void run() {
+
+        try {
+            System.out.println(simpleDivision(9, 3));
+            System.out.println(simpleDivision(5, 3));
+            System.out.println(simpleDivision(9, 9));
+            System.out.println(simpleDivision(9, -1));
+            System.out.println(simpleDivision(133, 0));
+            System.out.println(simpleDivision(133, 231));
+
+        } catch (MyException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Stop");
+        }
+    }
+
+    public static int simpleDivision(int x, int y) throws MyException {
+
+        if (y != 0) {
+            int result = x / y;
+            return result;
+        } else {
+            throw new MyException("My Exception Error");
+        }
+    }
+}
+
+class MyException extends Exception {
+
+    public MyException() {
+        super();
+    }
+
+    public MyException(String message) {
+        super();
+        System.out.println(message);
+    }
+}
