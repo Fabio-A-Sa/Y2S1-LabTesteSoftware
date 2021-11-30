6df635d13bb5ca62cb3039a36a633239e8f76fd2
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 22:33:14 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -1061,3 +1061,30 @@ public class RomulanClient extends AlienClient {
 
 }
 
+public class SmartStrategy implements OrderingStrategy {
+
+    private StringDrink drink;
+    private StringRecipe recipe;
+    private StringBar bar;
+
+    public void wants(StringDrink drink, StringRecipe recipe, StringBar bar) {
+
+        this.drink = drink;
+        this.recipe = recipe;
+        this.bar = bar;
+
+        if (bar.isHappyHour()) {
+            this.recipe.mix(this.drink);
+        }
+    }
+
+    public void happyHourStarted(StringBar bar) {
+        this.recipe.mix(this.drink);
+    }
+
+    public void happyHourEnded(StringBar bar) {
+        System.out.println("Not applicable");
+    }
+
+}
+
