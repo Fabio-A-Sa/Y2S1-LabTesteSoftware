a288fa8f2f10ec5784a0a95156b8af38525e20d0
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 22:28:54 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -769,3 +769,30 @@ public class ListAggregatorTest {
         Assertions.assertEquals(3, result);
     }
 }
+
+public class ListDeduplicatorTest {
+
+    /**
+     *  Não quero que o resultado de "deduplicate" dependa da classe sorter()
+     *  Então o deduplicate recebe um GenericListSorted, que tanto pode ser um objecto manipulado (stub) ou não
+     */
+    class StubSorter implements GenericListSorter {
+        @Override
+        public List<Integer> sort(List<Integer> list) {
+            List<Integer> stub = Arrays.asList(1, 2, 2, 4);
+            return stub;
+        }
+    }
+
+    @Test
+    public void deduplicate() {
+        List<Integer> list = Arrays.asList(1, 2, 4, 2);
+        List<Integer> expected = Arrays.asList(1, 2, 4);
+
+        ListDeduplicator deduplicator = new ListDeduplicator();
+        StubSorter stub = new StubSorter();
+        List<Integer> distinct = deduplicator.deduplicate(list, stub);
+
+        Assertions.assertEquals(expected, distinct);
+    }
+}
