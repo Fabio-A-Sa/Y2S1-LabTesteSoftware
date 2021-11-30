02275470f4097af7ffa8fb437c3cf67457dbd1b9
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 22:32:19 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -940,3 +940,32 @@ public interface Client extends BarObserver {
 
     void wants (StringDrink drink, StringRecipe recipe, StringBar bar);
 }
+
+public class FerengiClient extends AlienClient {
+
+    private OrderingStrategy strategy;
+
+    protected OrderingStrategy createOrderingStrategy() {
+        return new SmartStrategy();
+    }
+
+    public FerengiClient() {
+        super();
+        this.strategy = createOrderingStrategy();
+    }
+
+    @Override
+    public void happyHourStarted(Bar bar) {
+        strategy.happyHourStarted((StringBar) bar);
+    }
+
+    @Override
+    public void happyHourEnded(Bar bar) {
+        strategy.happyHourEnded((StringBar) bar);
+    }
+
+    @Override
+    public void wants(StringDrink drink, StringRecipe recipe, StringBar bar) {
+        strategy.wants(drink, recipe, bar);
+    }
+}
