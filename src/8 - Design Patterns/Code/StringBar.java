d81bb96d02dec8438f72ee92d51f1a9f85e2e8b4
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 20:24:49 +0000

    Get

--- src/8 - Design Patterns/Code/StringBar.java
+++ src/8 - Design Patterns/Code/StringBar.java
@@ -1,7 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringBar extends Bar {
 
     public StringBar() {
         super();
+    }
+
+    private StringRecipe gerRecipe() {
+
+        StringInverter si = new StringInverter();
+        StringCaseChanger cc = new StringCaseChanger();
+        StringReplacer sr = new StringReplacer('A', 'X');
+
+        List<StringTransformer> transformers = new ArrayList<>();
+        transformers.add(si);
+        transformers.add(cc);
+        transformers.add(sr);
+
+        StringRecipe recipe = new StringRecipe(transformers);
+        return recipe;
     }
 
 }
