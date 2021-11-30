6c9c1819ee89296a38a2840a0a67a4b5fbea406f
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 22:31:02 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -886,3 +886,46 @@ public abstract class AlienClient implements Client {
 
     protected abstract OrderingStrategy createOrderingStrategy();
 }
+
+public abstract class Bar {
+
+    private boolean happyHour;
+    private List<BarObserver> observers;
+
+    public Bar () {
+        this.happyHour = false;
+        this.observers = new ArrayList<>();
+    }
+
+    public boolean isHappyHour() {
+        return happyHour;
+    }
+
+    public void startHappyHour() {
+        this.happyHour = true;
+        notifyObservers();
+    }
+
+    public void endHappyHour() {
+        this.happyHour = false;
+        notifyObservers();
+    }
+
+    public void addObserver(BarObserver observer) {
+        this.observers.add(observer);
+    }
+
+    public void removeObserver(BarObserver observer) {
+        this.observers.remove(observer);
+    }
+
+    public void notifyObservers() {
+        for (BarObserver observer : observers) {
+            if (isHappyHour()) {
+                observer.happyHourStarted(this);
+            }  else {
+                observer.happyHourEnded(this);
+            }
+        }
+    }
+}
