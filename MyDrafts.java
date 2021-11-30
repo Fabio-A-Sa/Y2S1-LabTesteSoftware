8991b58c90fd8e7b5319e7273ee1a41ddb12d7ed
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 22:34:05 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -1154,4 +1154,21 @@ public class StringDrink {
     }
 }
 
+public class StringInverter implements StringTransformer {
+
+    public void execute(StringDrink drink) {
+
+        String invert = "";
+        for (int i = 0 ; i < drink.getText().length() ; i++) {
+            invert = drink.getText().charAt(i) + invert;
+        }
+
+        drink.setText(invert);
+    }
+
+    public void undo(StringDrink drink) {
+        execute(drink);
+    }
+}
+
 
