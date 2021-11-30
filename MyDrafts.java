0db31f96fd7561ced9c17fac8efb1efb4e48d779
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 22:24:16 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -375,3 +375,64 @@ class MyException extends Exception {
         System.out.println(message);
     }
 }
+
+class usingThreads {
+
+    public static void run() {
+
+        Model m = new Model();
+        View v = new View();
+
+        new Thread() {
+            public void run () {
+                while (m.a < 1000) {
+                    m.increment();
+                }
+            }
+        }.start();
+
+        new Thread() {
+            public void run() {
+                while (m.a < 1000) {
+                    v.draw(m);
+                }
+            }
+        }.start();
+    }
+}
+
+class Model {
+    int a = 0, b = 0;
+    public void increment() {
+        a++;
+        b++;
+    }
+}
+
+class View {
+    public void draw (Model m) {
+        System.out.println(m.a + " " + m.b);
+    }
+}
+
+
+
+abstract class Fruta {
+
+    String nome;
+    int tamanho;
+    public Fruta (String nome, int tamanho) {
+        this.nome = nome;
+        this.tamanho = tamanho;
+    }
+}
+
+class Banana extends Fruta {
+
+    String qualidade;
+    public Banana (String nome, int tamanho, String qualidade) {
+        super(nome, tamanho);
+        this.qualidade = qualidade;
+    }
+}
+
