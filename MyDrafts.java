3ab3a9201a320782ed646c9cf9e652cff0c2f93e
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 22:35:43 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -1186,3 +1186,40 @@ public class StringRecipe {
         }
     }
 }
+
+public class StringReplacer implements StringTransformer {
+
+    private char i;
+    private char f;
+
+    public StringReplacer (char i, char f) {
+        this.i = i;
+        this.f = f;
+    }
+
+    public void execute(StringDrink drink) {
+
+        String oldString = drink.getText();
+        String newString = "";
+
+        for (int i = 0 ; i < oldString.length() ; i++) {
+            char currentChar = oldString.charAt(i);
+            if (currentChar == this.i) currentChar = f;
+            newString += currentChar;
+        }
+        drink.setText(newString);
+    }
+
+    public void undo(StringDrink drink) {
+
+        String oldString = drink.getText();
+        String newString = "";
+
+        for (int i = 0 ; i < oldString.length() ; i++) {
+            char currentChar = oldString.charAt(i);
+            if (currentChar == this.f) currentChar = this.i;
+            newString += currentChar;
+        }
+        drink.setText(newString);
+    }
+}
