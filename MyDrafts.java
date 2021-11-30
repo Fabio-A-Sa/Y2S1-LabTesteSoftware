8c6881a67614609ba2ba69bdf5e7940627efe276
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 22:33:25 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -1088,3 +1088,28 @@ public class SmartStrategy implements OrderingStrategy {
 
 }
 
+public class StringBar extends Bar {
+
+    public StringBar() {
+        super();
+    }
+
+    public StringRecipe getRecipe() {
+
+        StringInverter si = new StringInverter();
+        StringCaseChanger cc = new StringCaseChanger();
+        StringReplacer sr = new StringReplacer('A', 'X');
+
+        List<StringTransformer> transformers = new ArrayList<>();
+        transformers.add(si);
+        transformers.add(cc);
+        transformers.add(sr);
+
+        StringRecipe recipe = new StringRecipe(transformers);
+        return recipe;
+    }
+
+    public void order (StringDrink drink,  StringRecipe recipe) {
+        recipe.mix(drink);
+    }
+}
