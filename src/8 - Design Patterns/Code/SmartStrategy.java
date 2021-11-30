85561a091e444af0a3a4b5c6c305109646addaa7
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 20:48:09 +0000

    New file

---
+++ src/8 - Design Patterns/Code/SmartStrategy.java
@@ -0,0 +1,7 @@
+public class SmartStrategy implements OrderingStrategy {
+
+    void wants(StringDrink drink, StringRecipe recipe, StringBar bar);
+    void happyHourStarted(StringBar bar);
+    void happyHourEnded(StringBar bar);
+
+}
