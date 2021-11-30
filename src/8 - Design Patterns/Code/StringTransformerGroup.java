7fbdb6a8d5a4e37d3f6d360364dbe49f06b88c94
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 19:33:01 +0000

    Design pattern file

---
+++ src/8 - Design Patterns/Code/StringTransformerGroup.java
@@ -0,0 +1,21 @@
+import jdk.dynalink.linker.LinkerServices;
+
+import java.util.List;
+
+public class StringTransformerGroup implements StringTransformer {
+
+    private List<StringTransformer> transformers;
+
+    public StringTransformerGroup (List<StringTransformer> transformers) {
+        this.transformers = transformers;
+    }
+
+    public void execute (StringDrink drink) {
+
+        for (StringTransformer transformer : transformers) transformer.execute(drink);
+    }
+
+    public void undo (StringDrink drink) {
+        System.out.println("Not");
+    }
+}
