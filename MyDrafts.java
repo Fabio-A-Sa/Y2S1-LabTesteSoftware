f2e311e90af29fbda471a1b4a69a5e7fe3e9eae5
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 22:29:38 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -833,3 +833,17 @@ public class ListFilterTest {
         Assertions.assertEquals(result, expected);
     }
 }
+
+public class ListSorterTest {
+
+    @Test
+    public void sort() {
+        List<Integer> list = Arrays.asList(3, 2, 6, 1, 4, 5, 7);
+        List<Integer> expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+
+        ListSorter sorter = new ListSorter();
+        List<Integer> sorted = sorter.sort(list);
+
+        Assertions.assertEquals(expected, sorted);
+    }
+}
