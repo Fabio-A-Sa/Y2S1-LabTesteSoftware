d4d11d93fc4bf938180fa50a2dc9cfe3faed87ee
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 22:32:37 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -1001,3 +1001,26 @@ public class HumanClient implements Client {
         bar.endHappyHour();
     }
 }
+
+public class ImpatientStrategy implements OrderingStrategy {
+
+    private StringDrink drink;
+    private StringRecipe recipe;
+    private StringBar bar;
+
+    public void wants(StringDrink drink, StringRecipe recipe, StringBar bar) {
+        this.drink = drink;
+        this.recipe = recipe;
+        this.bar = bar;
+        recipe.mix(drink);
+    }
+
+    public void happyHourStarted(StringBar bar) {
+        System.out.println("Not applicable");
+    }
+
+    public void happyHourEnded(StringBar bar) {
+        System.out.println("Not applicable");
+    }
+}
+
