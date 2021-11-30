f6fe6e60bb45f6009639d72336bc9ec2b74dc822
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 22:32:25 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -969,3 +969,35 @@ public class FerengiClient extends AlienClient {
         strategy.wants(drink, recipe, bar);
     }
 }
+
+public class HumanClient implements Client {
+
+    private OrderingStrategy strategy;
+    private StringDrink drink;
+    private StringRecipe recipe;
+    private StringBar bar;
+
+    public HumanClient() {
+        super();
+    }
+
+    public HumanClient(OrderingStrategy strategy) {
+        super();
+        this.strategy = strategy;
+    }
+
+    public void wants (StringDrink drink, StringRecipe recipe, StringBar bar) {
+        strategy.wants(drink, recipe, bar);
+        this.recipe = recipe;
+        this.bar = bar;
+        this.drink = drink;
+    }
+
+    public void happyHourStarted(Bar bar) {
+        this.recipe.mix(this.drink);
+    }
+
+    public void happyHourEnded(Bar bar) {
+        bar.endHappyHour();
+    }
+}
