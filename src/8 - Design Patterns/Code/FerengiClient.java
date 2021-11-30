257b6226223138699a9cba7a6d5594a1e6b9d74d
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 22:10:03 +0000

    Passo 5

--- src/8 - Design Patterns/Code/FerengiClient.java
+++ src/8 - Design Patterns/Code/FerengiClient.java
@@ -1,4 +1,29 @@
 public class FerengiClient extends AlienClient {
 
+    private OrderingStrategy strategy;
+
+    protected OrderingStrategy createOrderingStrategy() {
+        return new SmartStrategy();
+    }
+
+    public FerengiClient() {
+        super();
+        this.strategy = createOrderingStrategy();
+    }
+
+    @Override
+    public void happyHourStarted(Bar bar) {
+        strategy.happyHourStarted((StringBar) bar);
+    }
+
+    @Override
+    public void happyHourEnded(Bar bar) {
+        strategy.happyHourEnded((StringBar) bar);
+    }
+
+    @Override
+    public void wants(StringDrink drink, StringRecipe recipe, StringBar bar) {
+        strategy.wants(drink, recipe, bar);
+    }
 
 }
--- src/8 - Design Patterns/Code/RomulanClient.java
+++ src/8 - Design Patterns/Code/RomulanClient.java
@@ -1,3 +1,29 @@
 public class RomulanClient extends AlienClient {
-    
+
+    private OrderingStrategy strategy;
+
+    protected OrderingStrategy createOrderingStrategy() {
+        return new ImpatientStrategy();
+    }
+
+    public RomulanClient() {
+        super();
+        this.strategy = createOrderingStrategy();
+    }
+
+    @Override
+    public void happyHourStarted(Bar bar) {
+        strategy.happyHourStarted((StringBar) bar);
+    }
+
+    @Override
+    public void happyHourEnded(Bar bar) {
+        strategy.happyHourEnded((StringBar) bar);
+    }
+
+    @Override
+    public void wants(StringDrink drink, StringRecipe recipe, StringBar bar) {
+        strategy.wants(drink, recipe, bar);
+    }
+
 }
