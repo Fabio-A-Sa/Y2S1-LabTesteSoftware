e59ccd7d6a81b0fce4bcb9b7f65dceae455a1a7b
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 22:11:23 +0000

    All tests

--- src/8 - Design Patterns/Tests/StringDrinkTest.java
+++ src/8 - Design Patterns/Tests/StringDrinkTest.java
@@ -2,9 +2,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
-import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.quality.MockitoHint;
 
 public class StringDrinkTest {
 
