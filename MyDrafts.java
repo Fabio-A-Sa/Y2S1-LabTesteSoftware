94876d5d0ed2607e267029698759d3625f9af212
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 22:28:25 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -672,3 +672,20 @@ class FirstSpecification extends Specification {
         1 * notifier.notify('foo')
     }
 }
+
+public class DivisibleByFilterTest {
+
+    @Test
+    public void test() {
+
+        DivisibleByFilter divisibleByFilter = new DivisibleByFilter();
+        Assert.assertEquals(false, divisibleByFilter.accept(0));
+        Assert.assertEquals(true, divisibleByFilter.accept(-2));
+        Assert.assertEquals(true, divisibleByFilter.accept(3));
+        Assert.assertEquals(false, divisibleByFilter.accept(0));
+        Assert.assertEquals(true, divisibleByFilter.accept(2342));
+
+
+    }
+}
+
