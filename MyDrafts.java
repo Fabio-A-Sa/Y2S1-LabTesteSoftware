8432fac8fe06b6fc9e826433dc40b8e8fe32b324
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 22:33:37 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -1113,3 +1113,26 @@ public class StringBar extends Bar {
         recipe.mix(drink);
     }
 }
+
+public class StringCaseChanger implements StringTransformer {
+
+    public void execute(StringDrink drink) {
+
+        String oldString = drink.getText();
+        String newString = "";
+
+        for (int i = 0 ; i < oldString.length() ; i++) {
+            char currentChar = oldString.charAt(i);
+            if (Character.isLowerCase(currentChar)) {
+                currentChar = Character.toUpperCase(currentChar);
+            } else { currentChar = Character.toLowerCase(currentChar); }
+            newString += currentChar;
+        }
+
+        drink.setText(newString);
+    }
+
+    public void undo (StringDrink drink) {
+        execute(drink);
+    }
+}
