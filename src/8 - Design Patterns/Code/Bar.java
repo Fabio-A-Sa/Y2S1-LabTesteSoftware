e49adad09de063391c122b7dc56636caf14e0a38
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-11-30 19:31:16 +0000

    Design Pattern file

---
+++ src/8 - Design Patterns/Code/Bar.java
@@ -0,0 +1,45 @@
+import java.util.List;
+
+public abstract class Bar {
+
+    private boolean happyHour;
+    private List<BarObserver> observers;
+
+    public Bar () {
+        this.happyHour = false;
+        this.observers.clear();
+    }
+
+    public boolean isHappyHour() {
+        return happyHour;
+    }
+
+    public void startHappyHour() {
+        this.happyHour = true;
+        for (BarObserver observer : this.observers) {
+            observer.notify();
+        }
+    }
+
+    public void endHappyHour() {
+        this.happyHour = false;
+    }
+
+    public void addObserver(BarObserver observer) {
+        this.observers.add(observer);
+    }
+
+    public void removeObserver(BarObserver observer) {
+        this.observers.remove(observer);
+    }
+
+    public void notifyObservers() {
+        for (BarObserver observer : observers) {
+            if (isHappyHour()) {
+                observer.happyHourStarted(this);
+            }  else {
+                observer.happyHourEnded(this);
+            }
+        }
+    }
+}
