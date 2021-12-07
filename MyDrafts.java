72b5f5dea740b5a3df4c9a2442bea8199984ff02
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-07 22:40:35 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -1668,3 +1668,28 @@ public class Rectangle implements Object {
         graphics.drawLine(x, y + height, x, y);
     }
 }
+
+public class Shape {
+
+    private Object object;
+
+    public Shape(double x, double y, double width, double height) {
+        this.object = new Rectangle(x, y, width, height);
+    }
+
+    public Shape(double x, double y, double radius) {
+        this.object = new Circle(x, y, radius);
+    }
+
+    public double getArea() {
+        return object.getArea();
+    }
+
+    public double getPerimeter() {
+        return object.getPerimeter();
+    }
+
+    public void draw(GraphicFramework graphics) {
+        object.draw(graphics);
+    }
+}
