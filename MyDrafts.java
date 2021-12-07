d4e637c93fab3f07a9fa9868901a50763419f37a
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-07 22:39:47 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -1575,3 +1575,31 @@ public class OrderLine {
         this.quantity = quantity;
     }
 }
+
+public class Product {
+
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
+
