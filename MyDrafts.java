f0cbcb9a0402f8918d2b4e3ceb6112560690f468
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-07 22:40:14 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -1603,3 +1603,33 @@ public class Product {
     }
 }
 
+public class Circle implements Object {
+
+    private double x;
+    private double y;
+    private double radius;
+
+    public Circle(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    public void draw(GraphicFramework graphics) {
+        graphics.drawCircle(x, y, radius);
+    }
+}
+
+public interface GraphicFramework {
+    void drawLine(double x1, double y1, double x2, double y2);
+    void drawCircle(double x, double y, double radius);
+}
+
