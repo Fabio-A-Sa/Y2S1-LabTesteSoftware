7c249f58a03de8b0cf2958dc9ae369d0acd17e8c
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-07 22:45:22 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -1837,3 +1837,67 @@ public class Worker extends Person {
         return Objects.hash(this.getName(), this.getPhone(), username, password);
     }
 }
+
+public class Turtle {
+
+    private int row;
+    private int column;
+    private char direction;
+
+    public Turtle(int row, int column, char direction) {
+        this.row = row;
+        this.column = column;
+        this.direction = direction;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public char getDirection() {
+        return direction;
+    }
+
+    public void execute(char command) {
+
+        if (command == 'F') {
+            switch (direction) {
+                case 'N':
+                    row--;
+                    break;
+                case 'S':
+                    row++;
+                    break;
+                case 'W':
+                    column--;
+                    break;
+                case 'E':
+                    column++;
+                    break;
+                default:
+                    return;
+            }
+        } else {
+            switch (direction) {
+                case 'N':
+                    direction = command == 'L' ? 'W' : 'E';
+                    break;
+                case 'S':
+                    direction = command == 'L' ? 'E' : 'W';
+                    break;
+                case 'W':
+                    direction = command == 'L' ? 'S' : 'N';
+                    break;
+                case 'E':
+                    direction = command == 'L' ? 'N' : 'S';
+                    break;
+                default:
+                    return;
+            }
+        }
+    }
+}
