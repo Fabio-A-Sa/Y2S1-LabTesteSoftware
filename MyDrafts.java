ce1142d1fd5baf95f78c76dcecf55281a6d07f29
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-07 22:45:41 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -1901,3 +1901,65 @@ public class Turtle {
         }
     }
 }
+
+public class Tree {
+
+    public Date plantedAt;
+    public String locationLatitude;
+    public String locationLongitude;
+    public String locationName;
+    private List<Date> appraisalDates;
+
+    public Tree(Date plantedAt, String locationLatitude, String locationLongitude, String locationName) {
+        this.plantedAt = plantedAt;
+        this.setLocation(locationLatitude, locationLongitude, locationName);
+        this.appraisalDates = new ArrayList<>();
+    }
+
+    public void setLocation(String locationLatitude, String locationLongitude, String locationName) {
+        this.locationLatitude = locationLatitude;
+        this.locationLongitude = locationLongitude;
+        this.locationName = locationName;
+    }
+
+    public String toString() {
+        return "Tree planted at " + this.plantedAt.toString() + " in location " + this.locationLatitude + "," + this.locationLongitude + " (" + this.locationName + ")";
+    }
+
+    void addAppraisal(Date appraisalDate) {
+        this.appraisalDates.add(appraisalDate);
+    }
+
+    public List<Date> getAppraisals() {
+        return this.appraisalDates;
+    }
+
+    public boolean isNextAppraisalOverdue() {
+
+        Date latestAppraisalDate = new Date();
+
+        if (this.appraisalDates.size() > 0) {
+            latestAppraisalDate = this.appraisalDates.get(0);
+        }
+
+        for (Date appraisalDate : this.appraisalDates) {
+            if (latestAppraisalDate.before(appraisalDate)) {
+                latestAppraisalDate = appraisalDate;
+            }
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(latestAppraisalDate);
+        calendar.add(Calendar.MONTH, 3);
+
+        if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY)
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        else if (calendar.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY)
+            calendar.add(Calendar.DAY_OF_MONTH, 2);
+
+        Date nextAppraisalDate = calendar.getTime();
+
+        return nextAppraisalDate.before(new Date());
+    }
+}
+
