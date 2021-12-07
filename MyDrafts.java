ecfeeefe7e51af6379719bde60fa2dbd6374d706
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-07 22:40:27 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -1633,3 +1633,38 @@ public interface GraphicFramework {
     void drawCircle(double x, double y, double radius);
 }
 
+public interface Object {
+    public double getArea();
+    public double getPerimeter();
+    public void draw(GraphicFramework graphics);
+}
+
+public class Rectangle implements Object {
+
+    private double x;
+    private double y;
+    private double width;
+    private double height;
+
+    public Rectangle (double x, double y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+
+    public void draw(GraphicFramework graphics) {
+        graphics.drawLine(x, y, x + width, y);
+        graphics.drawLine(x + width, y, x + width, y + height);
+        graphics.drawLine(x + width, y + height, x, y + height);
+        graphics.drawLine(x, y + height, x, y);
+    }
+}
