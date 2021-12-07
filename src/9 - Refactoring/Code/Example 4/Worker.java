d966903c3bf8d7dd5f5f06791aa2be9938d93a6e
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-07 22:32:17 +0000

    Example 4 - Worker

---
+++ src/9 - Refactoring/Code/Example 4/Worker.java
@@ -0,0 +1,37 @@
+package pt.up.fe.ldts.example4;
+import java.util.Objects;
+
+// Smell --> Parallel Inheritance Hierarchies
+// https://refactoring.guru/smells/parallel-inheritance-hierarchies
+
+public class Worker extends Person {
+
+    private final String username;
+    private final String password;
+
+    public Worker(String name, String phone, String username, String password) {
+        super(name, phone);
+        this.username = username;
+        this.password = password;
+    }
+
+    public boolean login(String username, String password) {
+        return this.username.equals(username) && this.password.equals(password);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Worker worker = (Worker) o;
+        return Objects.equals(this.getName(), worker.getName()) &&
+                Objects.equals(this.getPhone(), worker.getPhone()) &&
+                Objects.equals(username, worker.username) &&
+                Objects.equals(password, worker.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getName(), this.getPhone(), username, password);
+    }
+}
