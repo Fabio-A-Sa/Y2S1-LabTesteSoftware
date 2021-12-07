c0f4d4b9fa074247044425d72a2c4d75e25dc041
Fabio_A_Sa <up202007658@edu.fe.up.pt>
2021-12-07 22:48:31 +0000

    Drafts update

--- MyDrafts.java
+++ MyDrafts.java
@@ -2238,3 +2238,36 @@ class TreeSpockTest extends Specification {
         !tree.isNextAppraisalOverdue()
     }
 }
+
+class SimpleOrderSpockTest extends Specification {
+
+    def 'Fixed Discount'() {
+        given:
+        def order = new SimpleOrder(100)
+
+        when:
+        order.setDiscount(new Discount(10))
+
+        then:
+        90 == order.getTotal()
+    }
+
+    def 'Percentage Discount'() {
+        given:
+        def order = new SimpleOrder(100)
+
+        when:
+        order.setDiscount(new Discount(0.2))
+
+        then:
+        80 == order.getTotal()
+    }
+
+    def 'No Discount'() {
+        given:
+        def order = new SimpleOrder(100)
+
+        expect:
+        100 == order.getTotal()
+    }
+}
